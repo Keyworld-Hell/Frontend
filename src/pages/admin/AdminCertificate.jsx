6b1a881e3b2dd663d250de69b76e05f5488fb76c
Sunny-jinn <rlawlsdn316@gmail.com>
2023-03-15 02:47:42 +0900

    refactor: Fix admin certificate page

--- src/pages/admin/AdminCertificate.jsx
+++ src/pages/admin/AdminCertificate.jsx
@@ -18,62 +18,85 @@ const AdminCertificate = () => {
         setActiveSubIndex={setActiveSubIndex}
       />
       <div className="admin-container container m-0">
-        <div className="admin-title flex admin-no-title f-20 fw-600">
-          <div className="admin-search">
+        <div className="admin-title admin-tech-search flex f-20 fw-600">
+          <div className="admin-search ">
             <input className="admin-search-input f-20" placeholder="검색" />
             <button className="admin-search-btn color-white">search</button>
           </div>
           <button className="admin-upload-btn  color-white">등록</button>
         </div>
         <div className="admin-tech-container flex">
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
           </div>
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
           </div>
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
           </div>
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
           </div>
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
           </div>
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
           </div>
-          <div className="admin-tech-box">
-            <div className="admin-tech-img">
-              <img src={img} alt={img} />
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
             </div>
-            <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
-            <button className="admin-tech-btn color-white">X</button>
+          </div>
+          <div className="admin-tech-contents flex">
+            <div className="admin-tech-box">
+              <div className="admin-tech-img">
+                <img src={img} alt={img} />
+              </div>
+              <div className="admin-tech-name flex f-20 fw-700">ISO-9001</div>
+              <button className="admin-tech-btn color-white">X</button>
+            </div>
           </div>
         </div>
       </div>
