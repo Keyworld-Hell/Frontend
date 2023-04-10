dcbe865ed7066d9f612a99209aeebdb81b25c19e
hyungjun KIM <hyungjunkim@hyungjunui-MacBookPro.local>
2023-04-11 00:20:43 +0900

    home.jsx update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,36 +33,44 @@ const Home = () => {
           </div>
           <div className="home-products-list flex">
             <div className="home-product">
-              <img src={container} alt={container} />
-              <div className="home-product-text ">
-                <div className="home-product-text-sub">보관함</div>
-                밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
-                공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
-              </div>
+              <a href={`/product/circularlock`} className={"color-white"}>
+                <img src={container} alt={container} />
+                <div className="home-product-text ">
+                  <div className="home-product-text-sub">보관함</div>
+                  밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
+                  공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
+                </div>
+              </a>
             </div>
             <div className="home-product">
-              <img src={auto} alt={auto} />
-              <div className="home-product-text ">
-                <div className="home-product-text-sub">자동화 기기</div>
-                밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
-                공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
-              </div>
+              <a href={`/product/circularlock`} className={"color-white"}>
+                <img src={auto} alt={auto} />
+                <div className="home-product-text ">
+                  <div className="home-product-text-sub">자동화 기기</div>
+                  밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
+                  공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
+                </div>
+              </a>
             </div>
             <div className="home-product">
-              <img src={train} alt={train} />
-              <div className="home-product-text ">
-                <div className="home-product-text-sub">기차 / 운송</div>
-                밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
-                공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
-              </div>
+              <a href={`/product/circularlock`} className={"color-white"}>
+                <img src={train} alt={train} />
+                <div className="home-product-text ">
+                  <div className="home-product-text-sub">기차 / 운송</div>
+                  밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
+                  공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
+                </div>
+              </a>
             </div>
             <div className="home-product">
-              <img src={product1} alt={product1} />
-              <div className="home-product-text ">
-                <div className="home-product-text-sub">키오스크</div>
-                밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
-                공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
-              </div>
+              <a href={`/product/circularlock`} className={"color-white"}>
+                <img src={product1} alt={product1} />
+                <div className="home-product-text ">
+                  <div className="home-product-text-sub">키오스크</div>
+                  밀폐성 및 단열성을 최우선으로 하는 판넬도어에 견고한 부속품을
+                  공급하여 강력한 밀페능력의 제품에 완성도를 높이고 있습니다.
+                </div>
+              </a>
             </div>
           </div>
         </div>
