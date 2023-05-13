dce01fa401bb03cc1ee67cc8d2a64cb232f51aa7
Sunny-jinn <rlawlsdn316@gmail.com>
2023-05-14 04:50:16 +0900

    feat: Add axios to notice register

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,8 +1,15 @@
 import React, { useRef, useState } from "react";
 import AdminNav from "../../components/layout/AdminNav";
 
+import axios from "axios";
+
+const SERVER_URL = "http://localhost:3000";
+
 const Admin = () => {
   const fileRef = useRef();
+  const dateRef = useRef();
+  const contentRef = useRef();
+
   const [fileName, setFileName] = useState("");
   const [fileImg, setFileImg] = useState();
   const [activeSubIndex, setActiveSubIndex] = useState(1);
@@ -17,6 +24,27 @@ const Admin = () => {
     };
   };
 
+  const postNoticeHandler = () => {
+    if (
+      !dateRef.current.value ||
+      !fileRef.current.files[0] ||
+      !contentRef.current.value
+    ) {
+      alert("빈칸을 채워주세요.");
+      return;
+    }
+
+    const formdata = new FormData();
+    formdata.append("date", dateRef.current.value);
+    formdata.append("file", fileRef.current.files[0]);
+    formdata.append("content", contentRef.current.value);
+
+    axios
+      .post(`${SERVER_URL}/adm/notice/new`, formdata)
+      .then((res) => alert("등록 완료!"))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="flex">
       <AdminNav
@@ -28,7 +56,12 @@ const Admin = () => {
       <div className="admin-container container m-0">
         <div className="admin-title flex f-20 fw-600">
           IMAGE
-          <button className="admin-upload-btn color-white">등록</button>
+          <button
+            className="admin-upload-btn color-white"
+            onClick={postNoticeHandler}
+          >
+            등록
+          </button>
         </div>
         <div className="admin-upload-img">
           {fileImg && <img src={fileImg} alt={fileImg} />}
@@ -52,14 +85,14 @@ const Admin = () => {
             </label>
           </div>
         </div>
-        <span className="f-20 fw-600">기간 설정</span>
+        <span className="f-20 fw-600">공지 기간 설정</span>
         <div className="admin-upload-date">
-          <input type="date" />
+          <input type="date" ref={dateRef} />
         </div>
         <div className="admin-upload-subtitle">
           <span className="f-20 fw-600">내용</span>
         </div>
-        <textarea className="admin-upload-content" />
+        <textarea className="admin-upload-content" ref={contentRef} />
       </div>
     </div>
   );
