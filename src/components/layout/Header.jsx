5be48feb0293e191f729f892538df3fe3ddf6b0b
Sunny-jinn <rlawlsdn316@gmail.com>
2023-05-17 23:25:27 +0900

    feat: Add mobile menu

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 import logo from "../../assets/img/logo.png";
 import us from "../../assets/img/us.png";
+import hamMenu from "../../assets/img/menu.svg";
 import HeaderMenu from "./HeaderMenu";
+import HeaderMenuNav from "./HeaderMenuNav";
 
 const Header = () => {
   const [isHover, setIsHover] = useState(false);
+  const [isNavClick, setIsNavClick] = useState(false);
 
   return (
     <>
@@ -55,8 +58,17 @@ const Header = () => {
             </ul>
           </nav>
         </div>
+        <div
+          className="header-hamburger cursor"
+          onClick={() => {
+            setIsNavClick((isNavClick) => !isNavClick);
+          }}
+        >
+          <img src={hamMenu} className="header-menu-icon m-0" alt="error" />
+        </div>
       </header>
       {isHover && <HeaderMenu setIsHover={setIsHover} />}
+      <HeaderMenuNav isNavClick={isNavClick} />
     </>
   );
 };
